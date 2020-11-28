5bbd7e733c6d407138384dbe01c2b21ea17e3110
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-11-29 00:55:08 +0100

    remitbot revamp

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,8 @@
 from tweepy import StreamListener, API, OAuthHandler, Stream
 import os
 from dotenv import load_dotenv
-from twitter_data import TwitterClient
 import logging
 load_dotenv()
-
-
-xyz = TwitterClient('@chopra__mudit',with_sentiment=True)
-xyz.set_query(input('Enter the word you want to search::  '))
-tweets = xyz.get_tweets()
-result = []
-for i in tweets:
-    if i['sentiment'] in set(['Negative', 'negative']):
-        result.append(i)
-result = result[0:4]
-print(result)
-            
 
 logger = logging.getLogger()
 
@@ -49,6 +36,7 @@ class Retweet(StreamListener):
 
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
+        print(tweet)
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
@@ -69,20 +57,16 @@ class Retweet(StreamListener):
     def on_error(self, status):
         logger.error(status)
 
-keywords = ["#TheraBotHelp", "Tweepy"] # filters could be hashtags, regular words, etc
+keywords = [
+    "#RemitBotHelp", "#remitbothelp",
+    "#RemitbotHelp", "#Remitbothelp",
+    "@Remitbothelp", "@remitbothelp",
+    "@RemitBotHelp"
+]
 
-
-def comment(api):
-    for i in result:
-        print(i)
-        api.update_status(
-            status="Hey! Hope you are doing good! Please know that we are here for you. You should visit a therapist if you feel like. Here is the link from which you can find a good one - https://www.goodtherapy.org/find-therapist.html",
-            in_reply_to_status_id=i['tweet_id']
-        )
 
 if __name__ == '__main__':
     api = create_app()
-    comment(api)
     tweets_listener = Retweet(api)
     stream = Stream(api.auth, tweets_listener)
     stream.filter(track=keywords, languages=["en"])
